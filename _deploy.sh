8d96fe7ce393952fa78bbddf0cc2b6e9de6ef7c5
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2025-06-07 22:16:02 +0200

    PkgDown improvement

--- _deploy.sh
+++ _deploy.sh
@@ -17,38 +17,29 @@ echo $PATH
 
 echo Running PkgDown in deployment directory...
 
-### Render the site
-if /usr/local/bin/R -e "pkgdown::build_site();" then
+/usr/local/bin/R -e "pkgdown::build_site();"
 
-# https://unix.stackexchange.com/questions/22726/how-to-conditionally-do-something-if-a-command-succeeded-or-failed
+echo Done with PkgDown. Copying old iROCK to a subdirectory of "public"...
 
-  echo Done with PkgDown. Copying old iROCK to a subdirectory of "public"...
+mkdir public/iROCK
+cp -R iROCK public
+mkdir public/img
+cp img/hex-logo.png public/img/hex-logo.png
 
-  mkdir public/iROCK
-  cp -R iROCK public
-  mkdir public/img
-  cp img/hex-logo.png public/img/hex-logo.png
+echo Copied old iROCK. Deleting old contents in publi HTML directory.
 
-  echo Copied old iROCK. Deleting old contents in publi HTML directory.
+rm -rf ~/rock.opens.science/*.*
+rm -rf ~/rock.opens.science/*
+rm -f ~/rock.opens.science/.htaccess
 
-  rm -rf ~/rock.opens.science/*.*
-  rm -rf ~/rock.opens.science/*
-  rm -f ~/rock.opens.science/.htaccess
+echo Deleted old contents. Copying new contents.
 
-  echo Deleted old contents. Copying new contents.
+### Copy website
+cp -RT public ~/rock.opens.science
 
-  ### Copy website
-  cp -RT public ~/rock.opens.science
+### Copy .htaccess
+cp -f .htaccess ~/rock.opens.science
 
-  ### Copy .htaccess
-  cp -f .htaccess ~/rock.opens.science
-
-  echo Done copying over new website.
-
-else
-
-  echo ERROR: PkgDown failed, doing nothing!
-
-fi
+echo Done copying over new website.
 
 echo - - - ENDING DEPLOYMENT SCRIPT at $(date) - - -
