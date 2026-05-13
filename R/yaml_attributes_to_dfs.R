fcfa8a16ac7d5b0be18533a1ff3e9f531364bebe
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2026-05-13 15:31:17 +0200

    added yaml_attributes_to_dfs()

---
+++ R/yaml_attributes_to_dfs.R
@@ -0,0 +1,52 @@
+#' Convert attributes from one or more YAML files to a list of data frames
+#'
+#' @param x A character vector with one or more file paths
+#'
+#' @returns A list of data frames
+#' @export
+#'
+#' @examples
+yaml_attributes_to_dfs <- function(x) {
+
+  ### To do: allow import of .rock files, too
+
+  ### For now, only accept YAML files
+
+  x <- grep("\\.ya?ml$", x, value=TRUE, ignore.case = TRUE);
+
+  yml <-
+    lapply(
+      x,
+      function(i) {
+        if (!file.exists(i)) {
+          warning("File '", i, "' does not exist! Skipping it.");
+          return(NA);
+        } else {
+          res <- yaml::read_yaml(i);
+          res <- res$ROCK_attributes;
+          return(res);
+        }
+      }
+    );
+  yml <- yml[!is.na(yml)];
+
+  dfs <-
+    lapply(
+      yml,
+      function(currentYamlObject) {
+        return(
+          rbind_df_list(
+            lapply(
+              currentYamlObject,
+              as.data.frame
+            )
+          )
+        );
+      }
+    );
+  names(dfs) <-
+    unlist(lapply(dfs, \(x) names(x)[1]));
+
+  return(dfs);
+
+}
